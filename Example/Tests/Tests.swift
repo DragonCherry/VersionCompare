dedf61e821378510a1bb8149f42c45513abd150b
DragonCherry <dragoncherry@naver.com>
2017-05-11 23:19:47 +0900

    Moved test code to Tests.swift file.

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -16,5 +16,16 @@ class Tests: XCTestCase {
     
     func testExample() {
         // This is an example of a functional test case.
+        
+        XCTAssertTrue(UIDevice.isSystemVersion(lessThan: "99.0.0"))
+        XCTAssertTrue(UIDevice.isSystemVersion(equalTo: UIDevice.current.systemVersion))
+        XCTAssertTrue(UIDevice.isSystemVersion(greaterThan: "3.5.99"))
+        XCTAssertTrue(UIDevice.isSystemVersion(lessThanOrEqualTo: "13.5.99"))
+        XCTAssertTrue(UIDevice.isSystemVersion(greaterThanOrEqualTo: UIDevice.current.systemVersion))
+        XCTAssertTrue(UIDevice.isVersion("10.0.1", equalTo: "10.0.1"))
+        XCTAssertTrue(UIDevice.isVersion("5.10.10", lessThan: "5.11.5"))
+        XCTAssertTrue(UIDevice.isVersion("1.0.0", greaterThan: "0.99.100"))
+        XCTAssertTrue(UIDevice.isVersion("0.5.3", lessThanOrEqualTo: "1.0.0"))
+        XCTAssertTrue(UIDevice.isVersion("0.5.29", greaterThanOrEqualTo: "0.5.3"))
     }
 }
